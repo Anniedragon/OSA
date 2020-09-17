0fd4237f75c0f8e6e026c4da6ca085d1d17c99fc
Anniedragon <54890451+Anniedragon@users.noreply.github.com>
2020-09-17 18:47:54 +0300

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -1,3 +1,5 @@
+//stack
+
 #include <iostream>
 using namespace std;
 int N = 10;
@@ -51,6 +53,70 @@ int main() {
 		else {
 			cout << "Element is: ";
 			one.pop();
+			cout << endl;
+		}
+	} while (true);
+	return 0;
+}
+
+//queue
+
+#include <iostream>
+using namespace std;
+int N = 10;
+class Queue {
+private:
+	int* arr;
+	int head;
+	int tail;
+public:
+	Queue() {
+		head = 0;
+		tail = 0;
+		arr = new int[N];
+	}
+	Queue(int m) {
+		head = 0;
+		tail = 0;
+		arr = new int[m];
+	}
+	~Queue() {
+		delete[] arr;
+	}
+	void add_item(int a) {
+		if (head == (tail + 1) % N) {
+			cout << "Queue is overfloated!" << endl;
+		}
+		else {
+			arr[tail] = a;
+			tail = (tail + 1) % N;
+		}
+	}
+	void extract_item() {
+		if (head != tail) {
+			cout << arr[head];
+			head = (head + 1) % N;
+		}
+		else cout << "Queue is empty!";
+	}
+};
+int main() {
+	int znach;
+	bool r;
+	cout << "How many elements will be in queue? - ";
+	cin >> N;
+	Queue one(N);
+	do {
+		cout << "Enter 1 to add or enter 0 to get out: ";
+		cin >> r;
+		if (r) {
+			cout << "Enter element: ";
+			cin >> znach;
+			one.add_item(znach);
+		}
+		else {
+			cout << "Element is: ";
+			one.extract_item();
 			cout << endl;
 		}
 	} while (true);
